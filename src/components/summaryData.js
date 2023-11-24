e1b9551e3a2fa95ff5ea8efeae5f76af1c4af148
krystiano13 <crystianotv@gmail.com>
2023-11-24 21:14:01 +0100

    split code into parts

---
+++ src/components/summaryData.js
@@ -0,0 +1,35 @@
+import iconReaction from "../assets/images/icon-reaction.svg";
+import iconMemory from "../assets/images/icon-memory.svg";
+import iconVerbal from "../assets/images/icon-verbal.svg";
+import iconVisual from "../assets/images/icon-visual.svg";
+
+export const summaryData = [
+    {
+        id : "reaction",
+        score : 80,
+        src : iconReaction,
+        alt : "reaction icon",
+        title : "Reaction",
+    },
+    {
+        id : "memory",
+        score : 92,
+        src : iconMemory,
+        alt : "memory icon",
+        title : "Memory",
+    },
+    {
+        id : "verbal",
+        score : 61,
+        src : iconVerbal,
+        alt : "verbal icon",
+        title : "Verbal",
+    },
+    {
+        id : "visual",
+        score : 72,
+        src : iconVisual,
+        alt : "visual icon",
+        title : "Visual",
+    },
+];
